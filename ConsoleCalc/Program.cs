cc5fcf845de73ef9fca70db38e13367842c578c0
IrinaMatisik <irinaMatisik1968@gmail.com>
2023-01-20 12:07:25 +0300

    Addition

---
+++ ConsoleCalc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int resAdd;
+            Console.Write("Input first: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input second: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            resAdd = a + b;
+            Console.WriteLine("Result addition: "+ resAdd);
+            Console.ReadKey();
+        }
+    }
+}
